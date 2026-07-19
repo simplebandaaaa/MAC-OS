1d0551b451059bb4472b9dcbfc2410acd975ac47
simplebandaaaa <simplebandaa@outlook.com>
2026-07-19 23:58:39 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 1. पुरानी किसी भी बची हुई लॉक फाइल्स को साफ करें
+rm -rf /var/run/xrdp/* /var/run/dbus/* /tmp/.X11-unix/X*
+
+# 2. D-Bus Daemon को बैकग्राउंड में चलाएं
+dbus-daemon --system --fork
+
+# 3. xrdp-sesman को शुरू करें
+xrdp-sesman
+
+# 4. xrdp को फ़ोरग्राउंड में चलाएं
+echo "Launching Pure GNOME RDP Server..."
+exec xrdp --nodaemon
